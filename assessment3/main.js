719d6640d1b5d6b3a08d8e9bb5fba601db21df3f
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-09-15 13:34:04 +0200

    main

--- assessment3/main.js
+++ assessment3/main.js
@@ -1,10 +1,12 @@
 import { fetchQuestion } from "./modules/question.api.js";
 import { createQuestion } from "./modules/question.component.js";
 
-const question = await fetchQuestion();
+(async () => {
+  const question = await fetchQuestion();
 
-const $question = createQuestion(question);
+  const $question = createQuestion(question);
 
-const $app = document.getElementById("app");
-$app.innerHTML = "";
-$app.appendChild($question);
+  const $app = document.getElementById("app");
+  $app.innerHTML = "";
+  $app.appendChild($question);
+})();
