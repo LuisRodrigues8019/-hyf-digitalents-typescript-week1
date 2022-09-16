f787c0163124ad8d3a2fe7cb6381378ca192cd62
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-09-16 15:17:36 +0200

    nex quetion

--- assessment3/modules/question.component.js
+++ assessment3/modules/question.component.js
@@ -26,7 +26,7 @@ const createQuestion = ({
         <h2 id="questionCategory">Category: ${category}</h2>
         </div>
         <div class="box4">
-        <form class="question-options" action="">
+        <form class="question-options" action="" id="form">
             <input type="radio" name="answer" id="answer1" value="${allAnswers[0]}">
             <label for="answer1">${allAnswers[0]}</label><br>
             <input type="radio" name="answer" id="answer2" value="${allAnswers[1]}">
@@ -49,22 +49,24 @@ const createQuestion = ({
     const answer = $form.querySelector('input[name="answer"]:checked').value;
 
     let $response = $question.querySelector(".question-response");
-    let nextQuestion = $question.querySelector(".next-question");
+    let $nextQuestion = $question.querySelector(".next-question");
+    console.log({ $response });
     if (!$response) {
       $response = document.createElement("p");
-      nextQuestion = document.createElement("button");
+      $nextQuestion = document.createElement("button");
+      console.log({ $response });
     }
-    
-
+    $nextQuestion.classList.add("next-question");
+    $nextQuestion.innerHTML = "next question";
+    $response.classList.add("question-response");
     $response.innerHTML = "Sorry wrong answer";
     if (answer === correctAnswer) {
       $response.innerHTML = "Correct !!";
     }
 
-    $question.appendChild(nextQuestion);
     $question.appendChild($response);
-
-    nextQuestion.addEventListener("click", () => {
+    $question.appendChild($nextQuestion);
+     $nextQuestion.addEventListener("click", () => {
       window.location.reload();
     });
   });
